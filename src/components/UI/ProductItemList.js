7c5ef832a5e62b6f3582e735afd6ec75c40c8f16
이승환 <tmdghks8642@gmail.com>
2023-05-16 10:23:04 +0900

    상품리스트 컴포넌트 만들기

---
+++ src/components/UI/ProductItemList.js
@@ -0,0 +1,66 @@
+import { useEffect, useState } from "react";
+import { styled } from "styled-components";
+
+
+const List = styled.section`
+display: inline-block;
+width: 16.7vw;
+height: 28vh;
+margin-right: 3rem;
+`
+const ListImg = styled.img`
+width: 16.7vw;
+height: 20vh;
+border-radius: 20px;
+`
+
+const ListTitle =styled.div`
+display:inline-block;
+font-weight: 800;
+margin-bottom: 0.5rem;
+`
+
+const PriceOrFallowers = styled.div`
+float: right;
+margin-bottom: 0.5rem;
+`
+const Pf = styled.div`
+float:right;
+position:relative;
+bottom:20px;
+
+`
+
+function ProductItem ({item,idx}) {
+
+    return (
+        <>
+        <List>
+            <ListImg src={
+                item[idx].image_url !== null ? item[idx].image_url : 
+                item[idx].brand_image_url
+                }/>
+            <ListTitle>{
+            item[idx].title !== null ? item[idx].title :
+            item[idx].brand_name 
+            }</ListTitle>
+            {
+             item[idx].follower !== null ? <PriceOrFallowers>관심고객수</PriceOrFallowers> :
+              item[idx].discountPercentage !== null ? <PriceOrFallowers >{item[idx].discountPercentage+`%`}</PriceOrFallowers> :
+              <PriceOrFallowers ><br></br></PriceOrFallowers>
+            }
+            {
+            item[idx].sub_title !== null ?  <div>{item[idx].sub_title}</div>: 
+            <div><br></br></div>
+            }
+            {
+            item[idx].follower !== null ? <Pf>11{item[idx].follower}</Pf>  : 
+            item[idx].price !==null ? <Pf>{item[idx].price+'원'}</Pf>:
+            <Pf><br></br></Pf>
+            }
+         </List>
+        </>
+    )
+}
+
+export default ProductItem
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,13 +1,67 @@
+import ProductItem from "../components/UI/ProductItemList"
+import { styled } from "styled-components"
+import { useState,useEffect } from "react"
 
+// 메인페이지 스타일
+const Mainpage = styled.main`
+height: 100vh;
+`
+// 상품 리스트 스타일
+const ProductsList = styled.div`
+display: inline-block;
+position:relative;
+left: 8vw;
+top: 2rem;
+`
 
+const ProductItems = styled.div`
+display: inline-block;
+`
+const ProductsListTitle = styled.h2`
+position:relative;
+`
+
+// 북마크 스타일
+const BookmarkList = styled.div`
+position:relative;
+left: 8vw;
+top: 60px;
+`
+const BookmarkListTitle = styled(ProductsListTitle)`
+`
 
 function Main (){
+const [item,SetItem] = useState()
+
+    useEffect(()=>{
+    fetch('http://cozshopping.codestates-seb.link/api/v1/products?count=4')
+    .then(rsp=> {
+        if(!rsp.ok){
+            throw new Error(rsp.statusText);
+        }
+        return rsp.json()
+    })
+    .then(json => SetItem(json))
+    .catch(error=> console.error(error))
+    },[]) 
+
+
 return (
-    <div>
+    <Mainpage>
+        <ProductsList>
+            <ProductsListTitle>상품 리스트</ProductsListTitle>
+                <ProductItems>
+                    {
+                        item&&item.map((el,idx)=> <ProductItem item={item} idx={idx}/>)
+                    }
+                </ProductItems>
+        </ProductsList>
 
+        <BookmarkList>
+        <BookmarkListTitle>북마크 리스트</BookmarkListTitle>
+        </BookmarkList>
 
-
-    </div>
+    </Mainpage>
 )
 }
 
